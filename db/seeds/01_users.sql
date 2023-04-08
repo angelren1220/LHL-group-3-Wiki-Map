66baab1dd43e068f979a109325053ac20b300e8a
Linuxe <angelren1220@gmail.com>
2023-04-08 16:51:58 -0400

    add seeds for tables

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,7 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- Users table seeds --
+INSERT INTO users (name, email, password)
+VALUES ('Angel', 'angel@lhl.mail', 'password');
+INSERT INTO users (name, email, password)
+VALUES ('Kade', 'kade@lhl.mail', 'password');
+INSERT INTO users (name, email, password)
+VALUES ('Oliver', 'oliver@lhl.mail', 'password');
---
+++ db/seeds/02_maps.sql
@@ -0,0 +1,7 @@
+-- maps table seeds --
+INSERT INTO maps (user_id, zoom, name)
+VALUES (1, 8, 'angel_map');
+INSERT INTO maps (user_id, zoom, name)
+VALUES (2, 8, 'kade_map');
+INSERT INTO maps (user_id, zoom, name)
+VALUES (3, 8, 'oliver_map');
