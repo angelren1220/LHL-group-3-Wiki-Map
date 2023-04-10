1a9167e4d0f055c6b6bfc104ef27c86881256987
OliverDanger <od.oliverdixon@gmail.com>
2023-04-10 16:43:44 +0000

    organizes multiple pins test

--- public/scripts/init_map.js
+++ public/scripts/init_map.js
@@ -3,7 +3,8 @@ let map;
 
 async function initMap() {
   // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
+  const position1 = { lat: -25.344, lng: 131.031 };
+  const position2 = { lat: -23.344, lng: 120.031 };
   // Request needed libraries.
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
@@ -11,22 +12,24 @@ async function initMap() {
 
   // The map, centered at Uluru
   map = new Map(document.getElementById("map"), {
-    zoom: 4,
-    center: position,
+    zoom: 3,
+    center: position1,
     mapId: "DEMO_MAP_ID",
   });
 
   // The marker, positioned at Uluru
-  // const marker = new marker({
-  //   map: map,
-  //   position: position,
-  //   title: "Uluru",
-  // });
-  new google.maps.Marker({
-    position: position,
+  const marker1 = new google.maps.Marker({
+    position: position1,
     map,
     title: "Hello World!",
   });
+
+  const marker2 = new google.maps.Marker({
+    position: position2,
+    map,
+    title: "Hi!",
+  });
 }
+
 
 initMap()
