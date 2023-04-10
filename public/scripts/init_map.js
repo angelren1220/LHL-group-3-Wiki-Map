dc7a6630d405dbf8f01d3d84390742bb833734d2
OliverDanger <od.oliverdixon@gmail.com>
2023-04-10 21:28:38 +0000

    info windows accept a title, description, and img url from each pin

--- public/scripts/init_map.js
+++ public/scripts/init_map.js
@@ -1,8 +1,56 @@
 // Initialize and add the map
 let map;
+let markers = [];
 
+//test data
+const pinData = [
+  {
+    position: {
+      lat: -25.344,
+      lng: 131.031
+    },
+    map: map,
+    title: "A",
+    description: "aaaaaa",
+    img: "https://d3hne3c382ip58.cloudfront.net/files/uploads/bookmundi/resized/cmsfeatured/uluru-rock-1511763600-785X440.jpg"
+  },
+  {
+    position: {
+      lat: -20.344,
+      lng: 120.031
+    }
+    ,
+    map: map,
+    title: "B",
+    description: "bbbbbb"
+  },
+  {
+    position: {
+      lat: -35.344,
+      lng: 140.031
+    },
+    map: map,
+    title: "C",
+    description: "cccccc"
+  }
+];
 
-
+const contentString = function(pin) {
+  let outputBuffer = "";
+  if (pin.title) {
+    outputBuffer += `<h1> ${pin.title} </h1>`;
+  };
+  if (pin.description) {
+    outputBuffer += `<p> ${pin.description}</p>`;
+  };
+  if (pin.img) {
+    outputBuffer += `<img src=${pin.img}>`;
+  }
+  if (!outputBuffer) {
+    outputBuffer = `<h1> Anonymous Pin <h1>`;
+  }
+  return outputBuffer;
+};
 
 async function initMap() {
   // The location of Uluru
@@ -18,43 +66,34 @@ async function initMap() {
     zoom: 3,
     center: position1,
     mapId: "DEMO_MAP_ID",
+    streetViewControl: false,
   });
 
-//test data
-  // const pinData = [
-  //   {
-  //     position: {
-  //       lat: -25.344,
-  //       lng: 131.031
-  //     },
-  //     map: map,
-  //     title: "A"
-  //   },
-  //   {
-  //     position: {
-  //       lat: -20.344,
-  //       lng: 120.031
-  //     }
-  //     ,
-  //     map: map,
-  //     title: "B"
-  //   },
-  //   {
-  //     position: {
-  //       lat: -35.344,
-  //       lng: 140.031
-  //     },
-  //     map: map,
-  //     title: "C"
-  //   }
-  // ];
 
+
+  //pinData is an array of objects. Each pin object must have: title, position (lat, lng), description, img
 
   for (let i = 0; i < pinData.length; i++) {
-    new google.maps.Marker({
+    const infowindow = new google.maps.InfoWindow({
+      content: contentString(pinData[i]),
+      ariaLabel: pinData[i].title,
+    });
+
+    const marker = new google.maps.Marker({
       position: pinData[i].position,
       map,
       title: pinData[i].title,
+      label: String(i + 1),
+    });
+
+    marker.addListener("click", () => {
+      infowindow.open({
+        anchor: marker,
+        map,
+      });
+    });
+    map.addListener(("click"), () => {
+      infowindow.close();
     });
   };
 
