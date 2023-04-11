05ea9b64b537899293e77bf4957c735d8339b89d
Linuxe <angelren1220@gmail.com>
2023-04-11 17:02:15 -0400

    add addPins

--- db/queries/pins.js
+++ db/queries/pins.js
@@ -13,22 +13,38 @@ const getPins = () => {
 };
 
 const getPinsWithUserId = (id) => {
-  if(!id) {
+  if (!id) {
     return null;
   }
   return db.
-  query(
-    `SELECT * FROM users WHERE users.id = $1`,
-    [Number(id)])
-  .then((result) => {
-    return result.rows[0];
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+    query(
+      `SELECT * FROM users WHERE users.id = $1`,
+      [Number(id)])
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+const addPins = (pin) => {
+  return db.
+    query(
+      `INSERT INTO pins (user_id, map_id, lag, lng, name, description, img_url)
+     VALUES ($1, $2, $3, $4, $5, $6, $7)
+     RETURNING *;`,
+      [Number(pin.user_id), Number(pin.map_id), Number(pin.lag), Number(pin.lng), pin.name, pin.description, pin.img_url])
+    .then(() => {
+      return "Add new pin successfully";
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 module.exports = {
   getPins,
-  getPinsWithUserId
+  getPinsWithUserId,
+  addPins
 };
