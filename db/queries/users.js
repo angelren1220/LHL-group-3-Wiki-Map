57b31ae76e3b061e8376d3f2948ea56114986fd1
Linuxe <angelren1220@gmail.com>
2023-04-11 17:01:41 -0400

    add getUserIdWithEmail

--- db/queries/users.js
+++ db/queries/users.js
@@ -7,7 +7,7 @@ const getUserWithEmail = (email) => {
 
   return db.
   query(
-    `SELECT * FROM users WHERE users.email = $1`,
+    `SELECT * FROM users WHERE users.email = $1;`,
     [email.toLowerCase()])
   .then((result) => {
     return result.rows[0];
@@ -23,7 +23,7 @@ const addUser = (user) => {
     `INSERT INTO users (name, email, password)
      VALUES ($1, $2, $3)
      RETURNING *;`,
-    [user.name, user.email, user.password])
+    [user.name, user.email.toLowerCase(), user.password])
   .then(() => {
     return "Add new user successfully";
   })
@@ -38,8 +38,26 @@ const getUserWithId = (id) => {
   }
   return db.
   query(
-    `SELECT * FROM users WHERE users.id = $1`,
+    `SELECT * FROM users WHERE users.id = $1;`,
     [Number(id)])
+  .then((result) => {
+    return result.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+};
+
+const getUserIDWithEmail = (email) => {
+  if(!email) {
+    return null;
+  }
+
+  return db.
+  query(
+    `SELECT id FROM users WHERE email = $1`,
+    [email.toLowerCase()]
+  )
   .then((result) => {
     return result.rows[0];
   })
@@ -51,5 +69,6 @@ const getUserWithId = (id) => {
 module.exports = {
   getUserWithEmail,
   addUser,
-  getUserWithId
+  getUserWithId,
+  getUserIDWithEmail
 };
