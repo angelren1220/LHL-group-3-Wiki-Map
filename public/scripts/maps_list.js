b580e91b4ce11147f0adf5a7557c22309feaea4b
Linuxe <angelren1220@gmail.com>
2023-04-11 20:43:56 -0400

    add little styles to maps list

--- public/scripts/maps_list.js
+++ public/scripts/maps_list.js
@@ -2,17 +2,17 @@ $(() => {
   $.ajax({
     method: 'GET',
     url: '/maps/user_list'
-  })  
+  })
     .then((data) => {
       const $mapsList = $('#maps');
       $mapsList.empty();
 
       for (const map of data) {
-        $(`<a>`).text(`id: ${map.id}, zoom; ${map.zoom}, name: ${map.name}`).attr("href", `/maps/${map.id}`).appendTo($mapsList);
+        $(`<a>`).text(`id: ${map.id}, zoom; ${map.zoom}, name: ${map.name}`).attr("href", `/maps/${map.id}`).addClass('black').appendTo($mapsList);
       }
     })
     .catch((err) => {
       console.log(err);
-    });;
+    });
 
 });
