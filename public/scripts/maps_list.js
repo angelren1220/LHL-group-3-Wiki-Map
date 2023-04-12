d0e5e3fd9d9147d42d91b49f79ef5621e43ff95d
Linuxe <angelren1220@gmail.com>
2023-04-11 20:25:33 -0400

    add links to maps list

--- public/scripts/maps_list.js
+++ public/scripts/maps_list.js
@@ -2,14 +2,17 @@ $(() => {
   $.ajax({
     method: 'GET',
     url: '/maps/user_list'
-  })
+  })  
     .then((data) => {
       const $mapsList = $('#maps');
       $mapsList.empty();
 
       for (const map of data) {
-        $(`<li class="maps">`).text(`${map.id}, ${map.name}`).appendTo($mapsList);
+        $(`<a>`).text(`id: ${map.id}, zoom; ${map.zoom}, name: ${map.name}`).attr("href", `/maps/${map.id}`).appendTo($mapsList);
       }
-    });
+    })
+    .catch((err) => {
+      console.log(err);
+    });;
 
 });
