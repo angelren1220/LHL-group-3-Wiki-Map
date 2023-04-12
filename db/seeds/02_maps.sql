3c59764423b35b2dc380d2f6b0b70b623178ca1d
OliverDanger <od.oliverdixon@gmail.com>
2023-04-12 19:21:02 +0000

    fixed merge conflict

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,20 +1,9 @@
 -- maps table seeds --
-<<<<<<< HEAD
 INSERT INTO maps (user_id, zoom, name, url)
 VALUES (1, 8, 'angel_map', 'https://github.com/angelren1220/LHL-group-3-Wiki-Map/blob/angelren/docs/map1.png?raw=true');
 INSERT INTO maps (user_id, zoom, name, url)
 VALUES (2, 8, 'kade_map', 'https://github.com/angelren1220/LHL-group-3-Wiki-Map/blob/angelren/docs/map2.png?raw=true');
 INSERT INTO maps (user_id, zoom, name, url)
 VALUES (3, 8, 'oliver_map', 'https://github.com/angelren1220/LHL-group-3-Wiki-Map/blob/angelren/docs/map3.png?raw=true');
-
-
 INSERT INTO maps (user_id, zoom, name, url)
 VALUES (3, 8, 'oliver_map2', 'https://github.com/angelren1220/LHL-group-3-Wiki-Map/blob/angelren/docs/map3.png?raw=true');
-=======
-INSERT INTO maps (user_id, zoom, name)
-VALUES (1, 3, 'angel_map');
-INSERT INTO maps (user_id, zoom, name)
-VALUES (2, 3, 'kade_map');
-INSERT INTO maps (user_id, zoom, name)
-VALUES (3, 3, 'oliver_map');
->>>>>>> affba6853f028491580e815ff36eecb950630d67
