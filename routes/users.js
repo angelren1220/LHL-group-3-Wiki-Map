a088c4dc26308bc4a774119ce393620f53770b9c
Linuxe <angelren1220@gmail.com>
2023-04-13 13:51:33 -0400

    add pages for invalid login and register

--- routes/users.js
+++ routes/users.js
@@ -19,11 +19,11 @@ router.post("/login", (req, res) => {
 
   userQueries.getUserWithEmail(email).then((user) => {
     if (!user) {
-      return res.send({ error: "error user not exists" });
+      return res.status(403).send("Invalid login! <a href='/users/login'>back</a>");
     }
 
     if (!bcrypt.compareSync(password, user.password)) {
-      return res.send({ error: "error password wrong" });
+      return res.status(403).send("Invalid login! <a href='/users/login'>back</a>");
     }
 
     req.session.user_id = user.id;
@@ -43,6 +43,10 @@ router.post("/logout", (req, res) => {
 // Register a new user
 router.post("/register", (req, res) => {
   const user = req.body;
+  if (!(user.name && user.email && user.password)) {
+    return res.status(400).send("Cannot register with empty string! <a href='/users/register'>back</a>");
+  }
+
   user.password = bcrypt.hashSync(user.password, 12);
   userQueries
     .addUser(user)
