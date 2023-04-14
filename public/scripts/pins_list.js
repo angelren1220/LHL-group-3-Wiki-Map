9e59641f29a9c3b1f3b326d095832e3f10753f9a
Linuxe <angelren1220@gmail.com>
2023-04-13 21:03:17 -0400

    add script for pins list

---
+++ public/scripts/pins_list.js
@@ -0,0 +1,18 @@
+$(() => {
+  $.ajax({
+    method: 'GET',
+    url: '/maps/api/pins'
+  })
+    .then((data) => {
+      const $pinsList = $('#pins');
+      $pinsList.empty();
+
+      for (const pins of data) {
+        $(`<li>`).text(`id: ${pins.id}, name: ${pins.name}`).addClass('black').appendTo($pinsList);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+});
