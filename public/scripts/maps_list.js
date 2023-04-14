77211e006183948dbdec0f5ace6b89a62fb7b091
Linuxe <angelren1220@gmail.com>
2023-04-13 20:57:21 -0400

    add button to view map's pins

--- public/scripts/maps_list.js
+++ public/scripts/maps_list.js
@@ -8,8 +8,9 @@ $(() => {
       $mapsList.empty();
 
       for (const map of data) {
-        $(`<a>`).text(`id: ${map.id}, name: ${map.name}`).attr("href", `/maps/${map.id}`).addClass('black').appendTo($mapsList);
+        $(`<a>`).text(`id: ${map.id}, name: ${map.name} View Map`).attr("href", `/maps/${map.id}`).addClass('black').appendTo($mapsList);
         $(`<form method="POST" action="/maps/delete/${map.id}"><button type="submit">Delete Map </button> </form>`).appendTo($mapsList);
+        $(`<form method="POST" action="/maps/pins/${map.id}"><button type="submit">Pins</button> </form>`).appendTo($mapsList);
       }
     })
     .catch((err) => {
